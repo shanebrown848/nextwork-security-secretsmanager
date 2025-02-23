6c3a9ca9235490a05e4ad46cdcf2944b183a594a
Natasha Ong <63530710+natashaongiscoding@users.noreply.github.com>
2025-02-24 09:20:17 +1300

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py - TEMPORARY for demonstration only
+# WARNING: This is NOT safe for production! We'll fix it with Secrets Manager.
+
+AWS_ACCESS_KEY_ID = "AKIAW3MEFRAZ6EDHD2SR"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_REGION = "us-east-2"
