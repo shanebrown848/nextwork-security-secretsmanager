9186d38e7651fdeb11f97743d278d69b1a5c2b25
Nat @ NextWork <natasha+aws@nextwork.org>
2025-03-03 13:53:21 +1300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py - TEMPORARY for demonstration only
 # WARNING: This is NOT safe for production! We'll fix it with Secrets Manager.
 
-AWS_ACCESS_KEY_ID = "YOUR_ACCESS_KEY_ID"
-AWS_SECRET_ACCESS_KEY = "YOUR_ACTUAL_ACCESS_KEY_ID"
+AWS_ACCESS_KEY_ID = "YOUR_ACTUAL_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY = "YOUR_ACTUAL_SECRET_ACCESS_KEY"
 AWS_REGION = "us-east-2"
